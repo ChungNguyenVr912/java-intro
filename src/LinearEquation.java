1cc6d3228749f85315deec5252566eb91b4fd30b
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-03 14:14:18 +0700

    Create LinearEquation.java

---
+++ src/LinearEquation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LinearEquation {
+    public static void main(String[] args){
+        System.out.println("Giai phuong trinh bac 1: y = a.x + b");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        double a = scanner.nextDouble();
+        System.out.println("Enter b: ");
+        double b = scanner.nextDouble();
+        System.out.println("Enter y: ");
+        double y = scanner.nextDouble();
+        if(a != 0){
+            double answer = (y - b)/a;
+            System.out.println("Nghiem cua pt: x = " + answer);
+        }else if(y == b){
+            System.out.println("Phuong trinh dung voi moi gia tri cua x");
+        }else {
+            System.out.println("Pt vo nghiem!");
+        }
+    }
+}
